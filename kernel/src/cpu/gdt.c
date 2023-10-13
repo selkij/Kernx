ec5416e154bbc1580dbb3871ac14a30463d34a61
Kiirx <kevan37000@hotmail.com>
2023-10-13 06:52:20 +0200

    🚧 GDT

---
+++ kernel/src/cpu/gdt.c
@@ -0,0 +1,5 @@
+//
+// Created by selkii on 10/11/23.
+//
+
+#include "gdt.h"
---
+++ kernel/src/include/cpu/gdt.h
@@ -0,0 +1,8 @@
+//
+// Created by selkii on 10/11/23.
+//
+
+#ifndef KERNX_GDT_H
+#define KERNX_GDT_H
+
+#endif //KERNX_GDT_H
