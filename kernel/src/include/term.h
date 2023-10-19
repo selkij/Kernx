c851476f0eb43c86366c115d03f172718110076f
Selkii <43090601+Kiirx@users.noreply.github.com>
2023-10-19 07:35:12 +0200

    ✨ Added Logging prefixes

--- kernel/src/include/term.h
+++ kernel/src/include/term.h
@@ -6,21 +6,20 @@
 #include "include/lib/string/str.h"
 #include "limine.h"
 #include <stdint.h>
-#include <stdbool.h>
 
-#define LOG_INFO_PREFIX "[ \033[0;34mINFO\033[0;34m ] "
-#define LOG_SUCCESS_PREFIX "[ \033[0;32mSUCCESS\033[0;34m ] "
-#define LOG_WARN_PREFIX "[ \033[0;33mWARN\033[0;34m ] "
-#define LOG_ERROR_PREFIX "[ \033[0;31mERROR\033[0;34m ] "
-#define LOG_FATAL_PREFIX "[ \033[0;35mFATAL\033[0;34m ] "
+#define LOG_INFO_PREFIX "[ \033[0;34mINFO\033[0;37m ] "
+#define LOG_SUCCESS_PREFIX "[ \033[0;32mSUCCESS\033[0;37m ] "
+#define LOG_WARN_PREFIX "[ \033[0;33mWARN\033[0;37m ] "
+#define LOG_ERROR_PREFIX "[ \033[0;31mERROR\033[0;37m ] "
+#define LOG_FATAL_PREFIX "[ \033[0;35mFATAL\033[0;37m ] "
 
 #define LOG_COMPACT_MODE 1
 
-#define LOG_INFO_PREFIX_COMPACT "[ \033[0;34m-\033[0;34m ]"
-#define LOG_SUCCESS_PREFIX_COMPACT "[ \033[0;32m-\033[0;34m ] "
-#define LOG_WARN_PREFIX_COMPACT "[ \033[0;33m-\033[0;34m ] "
-#define LOG_ERROR_PREFIX_COMPACT "[ \033[0;31m-\033[0;34m ] "
-#define LOG_FATAL_PREFIX_COMPACT "[ \033[0;35m-\033[0;34m ] "
+#define LOG_INFO_PREFIX_COMPACT "[ \033[0;34m-\033[0;37m ] "
+#define LOG_SUCCESS_PREFIX_COMPACT "[ \033[0;32m-\033[0;37m ] "
+#define LOG_WARN_PREFIX_COMPACT "[ \033[0;33m-\033[0;37m ] "
+#define LOG_ERROR_PREFIX_COMPACT "[ \033[0;31m-\033[0;37m ] "
+#define LOG_FATAL_PREFIX_COMPACT "[ \033[0;35m-\033[0;37m ] "
 
 void term_init(void);
 void printf(const char *str);
